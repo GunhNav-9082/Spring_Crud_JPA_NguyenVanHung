37da90e35c0f60821de559ba47c8e3da1bb89cb6
HungNg2k <chadkeeper2809@gmail.com>
2021-09-15 21:36:17 +0700

    commit crud

--- Spring_Crud_API_NguyenVanHung/src/main/java/com/example/spring_crud_api_nguyenvanhung/controller/HelloController.java
+++ Spring_Crud_API_NguyenVanHung/src/main/java/com/example/spring_crud_api_nguyenvanhung/controller/HelloController.java
@@ -3,9 +3,10 @@ package com.example.spring_crud_api_nguyenvanhung.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
     @GetMapping("/")
     public String helloWorld(){
-        return "Hello World!";
+        return "Van Hung Hello!";
     }
 }
